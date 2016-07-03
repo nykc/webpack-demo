7eb341fe3adb84e53e29ba07bcb5ea1e53d59c00
Derek Ackley <dackley@nykc.net>
2016-07-03 12:25:02 -0400

    webpack-validator for better error detection

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
+const validate = require('webpack-validator');
 
 const PATHS = {
   src: path.join(__dirname, 'src/assets/scripts'),
@@ -36,4 +37,4 @@ switch(process.env.npm_lifecycle_event) {
         config = merge(common, {});
 }
 
-module.exports = config;
+module.exports = validate(config);
